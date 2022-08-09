cea760fff4734c052cf81ff18ed1077d46f12324
testpageAN <alexis.nikolopoulos@hotmail.com>
2022-08-09 22:33:47 +0300

    9/8, 46
    Took 1 hour 7 minutes

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choises import price_choices, bedroom_choices, state_choices
 
 from listings.models import Listing
 from realtors.models import Realtor
@@ -10,6 +11,9 @@ def index(request):
 
     context = {
         'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
     }
 
     return render(request,'pages/index.html', context)
