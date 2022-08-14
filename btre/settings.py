3c74c494f04b79705ad2756ba5a8224d7c58c85c
testpageAN <87935364+testpageAN@users.noreply.github.com>
2022-08-14 11:54:41 +0300

    Update settings.py
    add local settings

--- btre/settings.py
+++ btre/settings.py
@@ -156,3 +156,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'myemail'
 EMAIL_HOST_PASSWORD = 'mypassword'
 EMAIL_USE_TLS = True
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
